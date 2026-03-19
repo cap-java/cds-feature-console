46d1d12f1cca98ae46e0da0076ae378e45600590
Pavlo Petrenko <pavlo.petrenko@sap.com>
2026-03-19 17:32:53 +0100

    test: Added more tests to increase coverage.

--- cds-feature-console/src/test/java/com/sap/cds/feature/console/info/collectors/LogCollectorTest.java
+++ cds-feature-console/src/test/java/com/sap/cds/feature/console/info/collectors/LogCollectorTest.java
@@ -8,15 +8,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
 class LogCollectorTest extends InfoCollectorTestBase {
 
-  private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(LogCollectorTest.class);
+  private static final Logger LOG = LoggerFactory.getLogger(LogCollectorTest.class);
 
   @AfterEach
   void resetLogEventsStore() {
@@ -27,14 +31,21 @@ class LogCollectorTest extends InfoCollectorTestBase {
     assertEquals(0, logs.size());
   }
 
-  @Test
-  void testUpdateLogLevelsToInfo() {
-    updateLogLevel(Level.INFO, LOG.getName());
+  static Stream<Arguments> logLevelFilterData() {
+    return Stream.of(
+        Arguments.of(Level.INFO, 1, 0),
+        Arguments.of(Level.ERROR, 0, 0));
+  }
+
+  @ParameterizedTest(name = "level={0}")
+  @MethodSource("logLevelFilterData")
+  void testUpdateLogLevelsFiltersLowerPriority(Level level, int expectedInfo, int expectedDebug) {
+    updateLogLevel(level, LOG.getName());
     LOG.info("info log");
     LOG.debug("debug log");
 
-    assertEquals(1, remoteMonitoringTestHandler.getLogEventsByLevel(Level.INFO).size());
-    assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.DEBUG).size());
+    assertEquals(expectedInfo, remoteMonitoringTestHandler.getLogEventsByLevel(Level.INFO).size());
+    assertEquals(expectedDebug, remoteMonitoringTestHandler.getLogEventsByLevel(Level.DEBUG).size());
   }
 
   @Test
@@ -67,78 +78,47 @@ class LogCollectorTest extends InfoCollectorTestBase {
     assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.DEBUG).size());
   }
 
-  @Test
-  void testUpdateLogLevelsToError() {
-    updateLogLevel(Level.ERROR, LOG.getName());
-
-    LOG.info("info log");
-    LOG.debug("debug log");
-    System.out.println("out");
-
-    assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.INFO).size());
-    assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.DEBUG).size());
+  static Stream<Arguments> loggerGroupData() {
+    return Stream.of(
+        Arguments.of("handlers", true, Level.WARN,
+            "com.sap.cds.services.impl.ServiceImpl", Level.INFO, 1),
+        Arguments.of("com.sap.cds.feature.console", false, Level.DEBUG,
+            null, Level.DEBUG, 0));
   }
 
-  @Test
-  void testUpdateLogLevelsForCdsLoggerGroup() {
-    Map<String, Object> logger = new HashMap<>();
-    logger.put("logger", "handlers"); // "handlers" is a CdsLoggerGroup value
-    logger.put("level", Level.WARN.toString());
-    logger.put("group", Boolean.TRUE);
+  @ParameterizedTest(name = "logger={0}, group={1}")
+  @MethodSource("loggerGroupData")
+  void testUpdateLogLevelsForLoggerGroup(
+      String loggerName, boolean isGroup, Level level,
+      String testLoggerName, Level resetLevel, int resetExpected) {
+    Map<String, Object> loggerConfig = new HashMap<>();
+    loggerConfig.put("logger", loggerName);
+    loggerConfig.put("level", level.toString());
+    loggerConfig.put("group", isGroup);
 
     List<Map<String, Object>> loggers = new ArrayList<>();
-    loggers.add(logger);
+    loggers.add(loggerConfig);
     Map<String, Object> data = new HashMap<>();
     data.put("loggers", loggers);
 
     CommandEvent update = new CommandEvent(LogCollector.COMMAND_UPDATE, data);
     remoteMonitoringService.emit(update);
 
-    Logger handlersLogger = LoggerFactory.getLogger("com.sap.cds.services.impl.ServiceImpl");
-    handlersLogger.warn("warn log for CdsLoggerGroup");
-    handlersLogger.info("info log for CdsLoggerGroup");
+    Logger testLogger = testLoggerName != null
+        ? LoggerFactory.getLogger(testLoggerName) : LOG;
+    logAtLevel(testLogger, level, "test log for group");
+    assertEquals(1,
+        remoteMonitoringTestHandler.getLogEventsByMessage("test log for group").size());
 
-    assertEquals(1, remoteMonitoringTestHandler.getLogEventsByLevel(Level.WARN).size());
-    assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.INFO).size());
-
-    // reset
     remoteMonitoringTestHandler.resetInfoEventsStore();
-    logger.put("level", null);
+    loggerConfig.put("level", null);
     remoteMonitoringService.emit(update);
-    handlersLogger.info("debug log for CdsLoggerGroup");
+    logAtLevel(testLogger, resetLevel, "test log after reset");
 
-    assertEquals(1, remoteMonitoringTestHandler.getLogEventsByLevel(Level.INFO).size());
+    assertEquals(resetExpected,
+        remoteMonitoringTestHandler.getLogEventsByMessage("test log after reset").size());
   }
 
-  @Test
-  void testUpdateLogLevelsForGroup() {
-    Map<String, Object> logger = new HashMap<>();
-    logger.put("logger", "com.sap.cds.feature.console");
-    logger.put("level", Level.DEBUG.toString());
-    logger.put("group", Boolean.FALSE);
-
-    List<Map<String, Object>> loggers = new ArrayList<>();
-    loggers.add(logger);
-    Map<String, Object> data = new HashMap<>();
-    data.put("loggers", loggers);
-
-    CommandEvent update = new CommandEvent(LogCollector.COMMAND_UPDATE, data);
-    remoteMonitoringService.emit(update);
-
-    LOG.debug("debug log for group");
-
-    assertEquals(1, remoteMonitoringTestHandler.getLogEventsByMessage("debug log for group").size());
-
-    // reset
-    remoteMonitoringTestHandler.resetInfoEventsStore();
-    logger.put("level", null);
-    remoteMonitoringService.emit(update);
-    LOG.debug("debug log for group");
-
-    assertEquals(0, remoteMonitoringTestHandler.getLogEventsByLevel(Level.DEBUG).size());
-  }
-
-  @SuppressWarnings("unchecked")
   private void updateLogLevel(Level level, String loggerName) {
     Map<String, Object> logger = new HashMap<>();
     logger.put("logger", loggerName);
@@ -155,4 +135,13 @@ class LogCollectorTest extends InfoCollectorTestBase {
     remoteMonitoringService.emit(update);
   }
 
+  private static void logAtLevel(Logger logger, Level level, String message) {
+    switch (level) {
+      case DEBUG -> logger.debug(message);
+      case INFO -> logger.info(message);
+      case WARN -> logger.warn(message);
+      case ERROR -> logger.error(message);
+      default -> logger.trace(message);
+    }
+  }
 }
